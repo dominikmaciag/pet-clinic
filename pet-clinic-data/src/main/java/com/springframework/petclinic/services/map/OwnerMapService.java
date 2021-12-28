331f6488628569efd65e43c576b1d548187f67e3
Dominik <poczta@dominikmaciag.com>
2021-12-28 14:57:54 +0100

    Updated profiles for JPA service, tested active profiles. Closes #45

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerMapService.java
@@ -2,11 +2,13 @@ package com.springframework.petclinic.services.map;
 
 import com.springframework.petclinic.model.Owner;
 import com.springframework.petclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
 
--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/VetMapService.java
@@ -2,11 +2,13 @@ package com.springframework.petclinic.services.map;
 
 import com.springframework.petclinic.model.Vet;
 import com.springframework.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
 
