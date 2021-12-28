c8352f47471dd13dd0fa85be4889b8eb2a03fbdd
Dominik <poczta@dominikmaciag.com>
2021-12-28 09:37:52 +0100

    Update vet to use JPA. Closes #35

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
@@ -11,4 +18,5 @@ public class Speciality extends BaseEntity {
     public void setDescription(String description) {
         this.description = description;
     }
+
 }
