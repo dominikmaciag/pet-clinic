56ba731c35c4afd5ed3e5d90aa7b964508822867
Dominik <poczta@dominikmaciag.com>
2022-01-02 19:02:21 +0100

    Tests for Owner JPA Service and Added Lombok. Closes #47 and #48

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Pet.java
@@ -1,10 +1,16 @@
 package com.springframework.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -27,43 +33,13 @@ public class Pet extends BaseEntity {
     private Set<Visit> visits = new HashSet<>();
 
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
         this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
         this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
         this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
-    }
 
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
     }
 }
---
+++ pet-clinic-data/src/test/java/com/springframework/petclinic/services/springdatajpa/OwnerSpringDataJpaServiceTest.java
@@ -0,0 +1,65 @@
+package com.springframework.petclinic.services.springdatajpa;
+
+
+import com.springframework.petclinic.model.Owner;
+import com.springframework.petclinic.repositories.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class OwnerSpringDataJpaServiceTest {
+
+    public static final String LAST_NAME = "Weston";
+
+    @Mock
+    OwnerRepository ownerRepository;
+
+    @InjectMocks
+    OwnerSpringDataJpaService service;
+
+    Owner returnOwner;
+
+    @BeforeEach
+    void setUp() {
+        returnOwner = Owner.builder().id(1l).lastName(LAST_NAME).build();
+    }
+
+    @Test
+    void findByLastName() {
+        when(ownerRepository.findByLastName(any())).thenReturn(returnOwner);
+        Owner weston = service.findByLastName(LAST_NAME);
+        assertEquals(LAST_NAME, weston.getLastName());
+        verify(ownerRepository).findByLastName(any());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+}
